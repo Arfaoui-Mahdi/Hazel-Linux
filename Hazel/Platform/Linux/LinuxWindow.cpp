97993ea9d315391eed86e76ac6c94573b167d541
Mahdi <mahdiarfaoui5477@gmail.com>
2024-02-07 22:46:46 +0100

    first steps in Dispatching events

--- Hazel/Platform/Linux/LinuxWindow.cpp
+++ Hazel/Platform/Linux/LinuxWindow.cpp
@@ -3,16 +3,26 @@
 #include "include/Core.hpp"
 #include "Log.hpp"
 
-namespace Hazel {
+#include "Events/ApplicationEvent.hpp"
+#include "Events/MouseEvent.hpp"
+#include "Events/KeyEvent.hpp"
+
+namespace Hazel
+{
 
 	static bool s_GLFWInitialized = false;
 
-	Window* Window::Create(const WindowProps& props)
+	static void GLFWErrorCallback(int error, const char *description)
+	{
+		HZ_CORE_ERROR("GLFW Error ({0}): {1}", error, description);
+	}
+
+	Window *Window::Create(const WindowProps &props)
 	{
 		return new LinuxWindow(props);
 	}
 
-	LinuxWindow::LinuxWindow(const WindowProps& props)
+	LinuxWindow::LinuxWindow(const WindowProps &props)
 	{
 		Init(props);
 	}
@@ -22,7 +32,7 @@ namespace Hazel {
 		Shutdown();
 	}
 
-	void LinuxWindow::Init(const WindowProps& props)
+	void LinuxWindow::Init(const WindowProps &props)
 	{
 		m_Data.Title = props.Title;
 		m_Data.Width = props.Width;
@@ -35,7 +45,7 @@ namespace Hazel {
 			// TODO: glfwTerminate on system shutdown
 			int success = glfwInit();
 			HZ_CORE_ASSERT(success, "Could not intialize GLFW!");
-
+			glfwSetErrorCallback(GLFWErrorCallback);
 			s_GLFWInitialized = true;
 		}
 
@@ -43,6 +53,82 @@ namespace Hazel {
 		glfwMakeContextCurrent(m_Window);
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
+
+		// Set GLFW callbacks
+		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow *window, int width, int height)
+								  {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			data.Width = width;
+			data.Height = height;
+
+			WindowResizeEvent event(width, height);
+			data.EventCallback(event); });
+
+		glfwSetWindowCloseCallback(m_Window, [](GLFWwindow *window)
+								   {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			WindowCloseEvent event;
+			data.EventCallback(event); });
+
+		glfwSetKeyCallback(m_Window, [](GLFWwindow *window, int key, int scancode, int action, int mods)
+						   {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			switch (action)
+			{
+				case GLFW_PRESS:
+				{
+					KeyPressedEvent event(key, 0);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					KeyReleasedEvent event(key);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_REPEAT:
+				{
+					KeyPressedEvent event(key, 1);
+					data.EventCallback(event);
+					break;
+				}
+			} });
+
+		glfwSetMouseButtonCallback(m_Window, [](GLFWwindow *window, int button, int action, int mods)
+								   {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			switch (action)
+			{
+				case GLFW_PRESS:
+				{
+					MouseButtonPressedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					MouseButtonReleasedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+			} });
+
+		glfwSetScrollCallback(m_Window, [](GLFWwindow *window, double xOffset, double yOffset)
+							  {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseScrolledEvent event((float)xOffset, (float)yOffset);
+			data.EventCallback(event); });
+
+		glfwSetCursorPosCallback(m_Window, [](GLFWwindow *window, double xPos, double yPos)
+								 {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseMovedEvent event((float)xPos, (float)yPos);
+			data.EventCallback(event); });
 	}
 
 	void LinuxWindow::Shutdown()
--- Hazel/include/Application.hpp
+++ Hazel/include/Application.hpp
@@ -6,6 +6,8 @@
 #include "Log.hpp"
 #include "LinuxWindow.hpp"
 
+#include "Events/ApplicationEvent.hpp"
+
 namespace Hazel {
 
     class Application {
@@ -15,7 +17,10 @@ namespace Hazel {
 
             void Run();
 
+            void OnEvent(Event& e);
+
         private:
+            bool OnWindowClose(WindowCloseEvent& e);
             std::unique_ptr<Window> m_Window;
             bool m_Running = true;
 
